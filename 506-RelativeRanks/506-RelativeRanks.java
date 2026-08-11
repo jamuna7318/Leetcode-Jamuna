9f0ec3c29e3bee2898d339f6480ebe3e026a0edb
Jamuna C <jamunachelladurai7@gmail.com>
2026-08-11 18:49:41 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.java
@@ -0,0 +1,28 @@
+// Last updated: 11/08/2026, 18:49:37
+import java.util.*;
+
+class Solution {
+
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = score[i];
+            arr[i][1] = i;
+        }
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++){
+            int index = arr[i][1];
+            if (i == 0)
+                answer[index] = "Gold Medal";
+            else if (i == 1)
+                answer[index] = "Silver Medal";
+            else if (i == 2)
+                answer[index] = "Bronze Medal";
+            else
+                answer[index] = String.valueOf(i + 1);
+        }
+        return answer;
+    }
+}
