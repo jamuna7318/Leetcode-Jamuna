26559917d4a9b0575523dc33604681dd7f1c3d12
Jamuna C <jamunachelladurai7@gmail.com>
2026-08-11 15:59:20 +0530

    Create 1944-TruncateSentence

---
+++ 1944-TruncateSentence/1944-TruncateSentence.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 15:59:16
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ' '){
+                count++;
+                if(count==k){
+                    return s.substring(0,i);
+                }
+            }
+        }
+        return s;
+    }
+}
