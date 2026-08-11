7314aaf8f6b5a63ff2f1617631a0861517950402
Jamuna C <jamunachelladurai7@gmail.com>
2026-08-11 18:52:57 +0530

    Create 68-TextJustification

---
+++ 68-TextJustification/68-TextJustification.java
@@ -0,0 +1,44 @@
+// Last updated: 11/08/2026, 18:52:53
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result=new ArrayList<>();
+        int i=0;
+        while(i<words.length){
+            int j=i;
+            int lineLength=0;
+            while (j < words.length && lineLength+words[j].length()+(j-i)<=maxWidth){
+                lineLength+=words[j].length();
+                j++;
+            }
+            int gaps=j-i-1;
+            StringBuilder line = new StringBuilder();
+            if(j==words.length || gaps==0){
+                for (int k=i;k<j;k++){
+                    line.append(words[k]);
+                    if (k!=j-1)
+                        line.append(" ");
+                }
+                while(line.length()<maxWidth)
+                    line.append(" ");
+            }else{
+                int totalSpaces=maxWidth-lineLength;
+                int spaceEach=totalSpaces/gaps;
+                int extra=totalSpaces%gaps;
+                for (int k=i;k<j;k++){
+                    line.append(words[k]);
+                    if(k!=j-1){
+                        for(int s=0;s<spaceEach;s++)
+                            line.append(" ");
+                        if(extra > 0){
+                            line.append(" ");
+                            extra--;
+                        }
+                    }
+                }
+            }
+            result.add(line.toString());
+            i=j;
+        }
+        return result;
+    }
+}
