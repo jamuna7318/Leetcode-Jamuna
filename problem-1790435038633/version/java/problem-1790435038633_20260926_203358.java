1f75c50a40ad48540fa8f93e0a16e123497ca839
Jamuna C <jamunachelladurai7@gmail.com>
2026-09-26 20:34:01 +0530

    Create

---
+++ problem-1790435038633/version/java/problem-1790435038633_20260926_203358.java
@@ -0,0 +1,15 @@
+// Last updated: 26/09/2026, 20:33:58
+1class Solution {
+2    public boolean canTransform(int[] source, int[] target) {
+3        if(source.length!=target.length){
+4            return false;
+5        }
+6        long sumSource=0;
+7        long sumTarget=0;
+8        for(int i=0;i<source.length;i++){
+9            sumSource+=source[i];
+10            sumTarget+=target[i];
+11        }
+12        return sumSource ==sumTarget;
+13    }
+14}
